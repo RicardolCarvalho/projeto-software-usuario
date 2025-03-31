2532cf5728bffc907b48a026a791399b2ba5afac
RicardolCarvalho <ricardolc2@al.insper.edu.br>
2025-03-31 10:41:55 -0300

    main

--- src/test/java/br/insper/iam/usuario/controller/UsuarioControllerTests.java
+++ src/test/java/br/insper/iam/usuario/controller/UsuarioControllerTests.java
@@ -1,5 +1,6 @@
 package br.insper.iam.usuario.controller;
 
+import br.insper.iam.usuario.CountUsuarioDTO;
 import br.insper.iam.usuario.Usuario;
 import br.insper.iam.usuario.UsuarioController;
 import br.insper.iam.usuario.UsuarioService;
@@ -44,8 +45,8 @@ public class UsuarioControllerTests {
     @Test
     void test_GetUsuarios() throws Exception {
         List<Usuario> usuarios = Arrays.asList(
-                new Usuario("João", "joao@example.com"),
-                new Usuario("Maria", "maria@example.com")
+                new Usuario("João", "joao@example.com", "ADMIN"),
+                new Usuario("Maria", "maria@example.com", "ADMIN")
         );
 
 
@@ -71,12 +72,23 @@ public class UsuarioControllerTests {
         ObjectMapper objectMapper = new ObjectMapper();
 
         mockMvc.perform(
-                    MockMvcRequestBuilders.post("/api/usuario")
-                            .content(objectMapper.writeValueAsString(usuario))
-                            .contentType(MediaType.APPLICATION_JSON)
+                        MockMvcRequestBuilders.post("/api/usuario")
+                                .content(objectMapper.writeValueAsString(usuario))
+                                .contentType(MediaType.APPLICATION_JSON)
                 )
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().json(objectMapper.writeValueAsString(usuario)));
 
+    }
+
+
+    @Test
+    void test_CountUsuarios() throws Exception {
+
+        CountUsuarioDTO countUsuarioDTO = new CountUsuarioDTO(5L);
+
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/usuario/count"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
